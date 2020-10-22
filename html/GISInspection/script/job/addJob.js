f23f0218164810bfe4eebac1122ec480b0d7817e
nonoSnow <nonosnow001@163.com>
2020-10-22 20:05:12 +0800

    新增工单的日期模块添加

--- html/GISInspection/script/job/addJob.js
+++ html/GISInspection/script/job/addJob.js
@@ -1,4 +1,5 @@
-
+var headList;  // 负责人信息
+var jobType; // 工单类型
 apiready = function() {
     var header = $api.byId('header');
     // 实现沉浸式状态栏效果
@@ -7,6 +8,7 @@ apiready = function() {
     $(".custom-popup-list li").each(function() {
       $(this).click(function() {
         $('#jobType').val($(this).text());
+        jobType = $(this).attr("value");
         $('.custom-popup-item').removeClass('color-598');
         $(this).addClass('color-598');
         onHidePopup();
@@ -17,12 +19,20 @@ apiready = function() {
         name: 'headList'
     }, function(ret, err) {
         alert(JSON.stringify(ret.value));
+        headList = ret.value
     });
+    // 创建日期
+    new Rolldate({
+				el: '#date-group1-3',
+				format: 'YYYY-MM-DD hh',
+				beginYear: 2000,
+				endYear: 2100
+			})
 }
 
 var popup = new auiPopup();
 function onHidePopup(){
-    popup.hide(document.getElementById("methodTypePop"));
+    popup.hide(document.getElementById("jobTypePop"));
 }
 
 function onOpenHead() {
